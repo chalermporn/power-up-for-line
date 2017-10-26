70da6e121dfcdaa72580f1b7b13db4aa7f2c9df1
Nakajima Kazuki <JP22274@w022571708172m.local>
2017-10-26 12:44:52 +0900

    test

--- app.js
+++ app.js
@@ -25,9 +25,16 @@ app.use('/', index);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// Allow CORS
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://trello.com");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 
 // error handler
