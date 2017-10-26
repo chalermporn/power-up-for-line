3dc1dac419a0983f38b239cb15eee6433dd70899
Nakajima Kazuki <JP22274@w022571708172m.local>
2017-10-26 15:33:55 +0900

    test

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
+var route_index = require('./routes/index');
 
 var app = express();
 
@@ -30,7 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 
-app.use('/', index);
+app.use('/', route_index);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -15,61 +15,63 @@ router.get('/', (req, res, next) => {
 
 // OAuth Success Callback
 router.get('/auth-success', (req, res, next) => {
-    // If we got code, we request access token.
-    if (req.query.code){
-        debug(`Authorization code is ${req.query.code}`);
-
-        if (!process.env.LINE_CLIENT_SECRET){
-            throw new Error(`LINE_CLIENT_SECRET not set.`)
-        }
-
-        // Request Access Token using code.
-        let query_strings = {
-            grant_type: "authorization_code",
-            code: req.query.code,
-            redirect_uri: "https://powerup-for-line.herokuapp.com/auth-success",
-            client_id: process.env.LINE_CLIENT_ID,
-            client_secret: process.env.LINE_CLIENT_SECRET
-        }
-
-        // Construct token url.
-        let token_url = "https://notify-bot.line.me/oauth/token?grant_type=" + grant_type + "&code=" + code + "&redirect_uri=" + redirect_uri + "&client_id=" + client_id + "&client_secret=" + client_secret;
-        let index = 0;
-        for (let q of query_strings){
-            token_url = token_url + query_strings[index] + "=" + encodeURIComponent(q) + "&";
-            index++;
-        }
-        token_url = token_url.slice(0, -1);
-        debug(`Token URL is ${token_url}`);
-
-        let headers = {
-            "Content-Type": "application/x-www-form-urlencoded"
-        }
-
-        return request.postAsync({
-            url: token_url,
-            headers: headers,
-            json: true
-        }).then((response) => {
-            if (response.Status !== 200){
-                debug(`Faield to get access token.`);
-                return res.sendStatus(response.body.Status);
-            }
-            if (!response.body.access_token){
-                debug(`Faield to get access token.`);
-                return res.sendStatus(400);
-            }
-
-            /*
-            return res.json({
-                access_token: response.body.access_token
-            });
-            */
-            return res.render('auth-success', {
-                access_token: response.body.access_token
-            });
-        });
+    if (!req.query.code){
+        return res.sendStatus(400);
     }
+
+    // If we got code, we request access token.
+    debug(`Authorization code is ${req.query.code}`);
+
+    if (!process.env.LINE_CLIENT_SECRET){
+        throw new Error(`LINE_CLIENT_SECRET not set.`)
+    }
+
+    // Request Access Token using code.
+    let query_strings = {
+        grant_type: "authorization_code",
+        code: req.query.code,
+        redirect_uri: "https://powerup-for-line.herokuapp.com/auth-success",
+        client_id: process.env.LINE_CLIENT_ID,
+        client_secret: process.env.LINE_CLIENT_SECRET
+    }
+
+    // Construct token url.
+    let token_url = "https://notify-bot.line.me/oauth/token?grant_type=" + grant_type + "&code=" + code + "&redirect_uri=" + redirect_uri + "&client_id=" + client_id + "&client_secret=" + client_secret;
+    let index = 0;
+    for (let q of query_strings){
+        token_url = token_url + query_strings[index] + "=" + encodeURIComponent(q) + "&";
+        index++;
+    }
+    token_url = token_url.slice(0, -1);
+    debug(`Token URL is ${token_url}`);
+
+    let headers = {
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+
+    return request.postAsync({
+        url: token_url,
+        headers: headers,
+        json: true
+    }).then((response) => {
+        if (response.Status !== 200){
+            debug(`Faield to get access token.`);
+            return res.sendStatus(response.body.Status);
+        }
+        if (!response.body.access_token){
+            debug(`Faield to get access token.`);
+            return res.sendStatus(400);
+        }
+
+        /*
+        return res.json({
+            access_token: response.body.access_token
+        });
+        */
+        return res.render('auth-success', {
+            access_token: response.body.access_token
+        });
+    });
 });
 
 module.exports = router;
